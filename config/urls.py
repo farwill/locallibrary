5278294dae4e068705e4bcc342fca32e5ec749ba
Jack Lin <farwill@gmail.com>
2023-05-18 08:46:07 +0000

    add catalog urls.py

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path("users/", include("locallibrary.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path("catalog/", include("locallibrary.catalog.urls", namespace="catalog")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     # Static file serving when using Gunicorn + Uvicorn for local web socket development
