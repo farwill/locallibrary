934a2949804a4fe7d73417bfa3fdf950f985c17c
Jack Lin <farwill@gmail.com>
2023-05-18 22:38:53 +0000

    Part 5 finish

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,5 +1,8 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+
+from . import views
 
 app_name = "catalog"
-urlpatterns = []
+urlpatterns = [
+    path("", views.index, name="index"),
+]
