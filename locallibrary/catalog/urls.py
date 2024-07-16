f914fc6f16b429aaf45baa384711d7f3d82f1d41
Jack Lin <farwill@gmail.com>
2024-07-16 15:53:18 +0800

    Part 9 partial

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("author/<int:pk>", views.AuthorDetailView.as_view(), name="author-detail"),
     path("mybooks/", views.LoanedBooksByUserListView.as_view(), name="my-borrowed"),
     path("borrowed/", views.LoanedBooksAllListView.as_view(), name="all-borrowed"),  # Added for challenge
+    path("book/<uuid:pk>/renew/", views.renew_book_librarian, name='renew-book-librarian'),
 ]
